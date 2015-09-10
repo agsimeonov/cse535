df7f5e76e2e0483ea029efd0604f02db7a6e62b0
Alexander Simeonov <agsimeon@buffalo.edu>
2015-09-10 15:41:10 -0400

    Crawler can now store tweets and has a proper status counter.

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 import sys
@@ -5,6 +6,10 @@ import traceback
 
 import tweepy
 
+
+ENGLISH = 'en'
+GERMAN =  'de'
+RUSSIAN = 'ru'
 
 # Make sure we have the correct command line arguments
 if len(sys.argv) != 6:
@@ -37,12 +42,7 @@ else:
   print "You have successfully logged on as: " + api.me().screen_name
 
 # Initialize variables
-topic = ['politics', 'elections', 'political rallies', 'policy changes', 'government']
-topic += ['politik', 'wahlen', 'politische kundgebungen', 'regierung']
-
-# extension = '.txt'
-# maxCount = 1000
-# flushCount = 100
+topic = ['politics', 'elections', 'government', 'politik', 'wahlen', 'regierung']
 
 #Create output directory
 directory = sys.argv[5]
@@ -54,92 +54,31 @@ if not os.path.exists(directory):
 
 class StreamListener(tweepy.StreamListener):
   def __init__(self):
-    self.count = 0
-    self.actualCount = 0
+    self.decount = 0
+    self.encount = 0
+    self.rucount = 0
     
   def on_data(self, data):
     decoded = json.loads(data)
-    username = ('%s' % (decoded['user']['screen_name'])).lower()
-    followersCount = ('%i' % (decoded['user']['followers_count']))
-    tweet = ('%s' % (decoded['text'].encode('utf-8', 'ignore')))
     language = ('%s' % (decoded['lang'].encode('utf-8', 'ignore')))
-#         mentions = ''
-#         hashtags = ''
-#         
-#         # Collect coordinates when possible (longitude then latitude)
-#         if decoded['coordinates'] is not None:
-#             writer.writerow([decoded['coordinates']['coordinates'][0],
-#                             decoded['coordinates']['coordinates'][1]])
-#         
-#         # Remove quotations
-#         tweet = tweet.replace('"', '')
-#         
-#         # Remove links
-#         tweet = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*',
-#                        '', tweet)
-#         
-#         # Cleanup unneeded retweet characters
-#         tweet = ' '.join(tweet.split('\n')).lower()
-#         if tweet.startswith('rt @'):
-#             tweet = tweet.replace(tweet[:3], '')
-#         tweet = tweet.replace(':', ' ')
-#         
-#         # Clean up symbols and numbers
-#         tweet = re.sub('[^a-zA-Z@#\n]', ' ', tweet)
-#         
-#         # Make sure mentions and hashtags are separated by a space
-#         tweet = re.sub(r"(\w)(['@|#'])", r"\1 \2", tweet)
-#         
-#         # Truncate consecutive hashtags to 1
-#         tweet = re.sub(r'(#)\1+', r'\1', tweet)
-#         
-#         # Collect mention usernames and clean them up from the tweet
-#         for mention in tweet.split():
-#             if mention.startswith('@'):
-#                 if len(mention) != 1:
-#                     cleanMention = mention.replace('@', ' ', 1)
-#                     cleanMention = re.sub('[^a-zA-Z0-9_]', '', cleanMention)
-#                     if len(cleanMention) <= 15:
-#                         mentions = mentions + ' ' + cleanMention
-#                 tweet = tweet.replace(mention, '')
-#     
-#         # Collect hashtags and clean them up from the tweet
-#         for hashtag in tweet.split():
-#             if hashtag.startswith('#'):
-#                 if len(hashtag) != 1:
-#                     cleanHashtag = re.sub('[^a-zA-Z0-9#]', '', hashtag)
-#                     hashtags = hashtags + ' ' + cleanHashtag
-#                 tweet = tweet.replace(hashtag, '')
-#         
-#         # Remove words with less than 2 characters
-#         tweet = ' '.join(word for word in tweet.split() if len(word) > 2)
-# 
-#         # Truncate/remove unneeded spaces
-#         tweet = ' '.join(tweet.split())
-#         mentions = ' '.join(mentions.split())
-#         hashtags = ' '.join(hashtags.split())
-#         
-#         # Split data into different files
-#         if count == maxCount:
-#             file.close()
-#             count = 0
-#             file = open(directory +
-#                         time.strftime('%Y%m%d-%H%M%S') + extension, 'w')
-#         
-#         # Write data to file
-#         file.write('%s | ' % username)
-#         file.write('%s | ' % followersCount)
-#         file.write('%s | ' % mentions)
-#         file.write('%s | ' % hashtags)
-#         file.write('%s |\n' % tweet)
-# 
-        # Flush and print current status
-    self.count = self.count + 1
-    self.actualCount = self.actualCount + 1
-#         if (count % flushCount) == 0:
-#             file.flush()
-#             coordinates.flush()
-    sys.stdout.write('\r\x1b[K' + 'Tweets processed: ' + str(self.actualCount))
+    utc = datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%S%fZ')
+    path = os.path.join(directory, language + utc + '.json')
+
+  # Store the data
+    with open(path, 'w') as out:
+      out.write(data)
+
+  # Show current status
+    if language == ENGLISH:
+      self.encount += 1
+    elif language == GERMAN:
+      self.decount += 1
+    else:
+      self.rucount += 1
+
+    sys.stdout.write('\r\x1b[K' + 'English processed: ' + str(self.encount) + ' ' +
+                                  'German processed: '  + str(self.decount) + ' ' +
+                                  'Russian processed: ' + str(self.rucount) + '\n')
     sys.stdout.flush()
 
     return True
@@ -155,12 +94,10 @@ listener = StreamListener()
 stream = tweepy.Stream(auth, listener)
 
 try:
-  stream.filter(track=topic, languages=['ru', 'en', 'de'])
+  stream.filter(track=topic, languages=[ENGLISH, GERMAN, RUSSIAN])
 except KeyboardInterrupt:
   pass
-#     file.close()
 except:
-#     file.close()
   traceback.print_exc()
 finally:
-  print "\nGoodbye!"
+  print "Goodbye!"
