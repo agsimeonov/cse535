2fa4a35896a33a726cbcf38a55213110beb72a21
Alexander Simeonov <agsimeon@buffalo.edu>
2015-09-14 21:02:49 -0400

    Equlibrium now equalizes separately for each day.

--- Crawler/equilibrium.py
+++ Crawler/equilibrium.py
@@ -7,6 +7,8 @@ ENGLISH = 'en'
 GERMAN  = 'de'
 RUSSIAN = 'ru'
 
+total = 0
+
 # Make sure we have the correct command line arguments
 if len(sys.argv) != 3:
   print "Please provide command line arguments as follows:"
@@ -21,29 +23,47 @@ outdir = sys.argv[2]
 if not os.path.exists(outdir): 
   os.makedirs(outdir)
 
-# Sort file paths into bins
-dictionary = {ENGLISH: [], GERMAN: [], RUSSIAN: []}
+# Split file names into days
+days = {}
 
 for name in os.listdir(indir):
-  if name.startswith(ENGLISH):
-    dictionary.get(ENGLISH).append(name)
-  elif name.startswith(GERMAN):
-    dictionary.get(GERMAN).append(name)
+  day = int(name[2:10])
+
+  if day not in days:
+    days[day] = [name]
   else:
-    dictionary.get(RUSSIAN).append(name)
+    days[day].append(name)
 
-# Get size of each bin
-ensize = len(dictionary.get(ENGLISH))
-desize = len(dictionary.get(GERMAN))
-rusize = len(dictionary.get(RUSSIAN))
+# Split each day into language specific bins
+for day in days:
+  # Sort file names into bins
+  bins = {ENGLISH: [], GERMAN: [], RUSSIAN: []}
 
-# Get size of smallest bin
-minimum = min([ensize, desize, rusize])
+  for name in days[day]:
+    if name.startswith(ENGLISH):
+      bins.get(ENGLISH).append(name)
+    elif name.startswith(GERMAN):
+      bins.get(GERMAN).append(name)
+    else:
+      bins.get(RUSSIAN).append(name)
 
-# Create an equilibrium of tweets for each language
-for names in dictionary.values():
-  # Make sure to get a random sample
-  random.shuffle(names)
-  for name in names[:minimum]:
-    # Symlinks should be sufficient
-    os.symlink(os.path.join(indir, name), os.path.join(outdir, name))
+  # Get size of each bin
+  ensize = len(bins.get(ENGLISH))
+  desize = len(bins.get(GERMAN))
+  rusize = len(bins.get(RUSSIAN))
+
+  # Get size of smallest bin
+  minimum = min([ensize, desize, rusize])
+  
+  total += minimum * 3
+  print str(day) + " - Daily total: " + str(minimum * 3) + "\tPer language: " + str(minimum)
+    
+  #Create an equilibrium of tweets for each language
+  for names in bins.values():
+    # Make sure to get a random sample
+    random.shuffle(names)
+    for name in names[:minimum]:
+      # Symlinks should be sufficient
+      os.symlink(os.path.join(indir, name), os.path.join(outdir, name))
+
+print "Total: " + str(total)
