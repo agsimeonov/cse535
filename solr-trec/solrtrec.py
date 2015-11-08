d6bfcf8645585ac0e0ddedacb2ff12b93632c94d
Alexander Simeonov <agsimeon@buffalo.edu>
2015-11-08 01:14:58 -0500

    Finished up solr to trec script.

--- solr-trec/solrtrec.py
+++ solr-trec/solrtrec.py
@@ -1,23 +1,42 @@
 # -*- coding: utf-8 -*-
 
-import codecs
+import json
 from sys import argv, exit
-from urllib2 import quote
+from urllib2 import urlopen
+
+import goslate
 
 
 # Make sure we have the correct command line arguments
 if len(argv) != 5:
   print "Please provide command line arguments as follows:"
-  print "python solrtrec.py <Query File> <Output File> <Domain> <Core>"
+  print "python solrtrec.py <Formatted Query File> <Output File> <Domain> <Core>"
   exit(0)
 
 port = 8983
 prepend = "http://" + argv[3] + ":" + str(port) + "/solr/" + argv[4] + "/select?q="
 append = '&fl=id%2Cscore&wt=json&indent=true&rows=1000'
 urls = [] # (id, url) format
+model='default'
 
-with codecs.open(argv[1],encoding='utf-8') as queries:
+gs = goslate.Goslate(service_urls=['http://translate.google.ca',
+                                   'http://translate.google.nl',
+                                   'http://translate.google.co.uk'])
+
+with open(argv[1]) as queries:
   for query in queries.read().splitlines():
     split = query.split(' ', 1)
-    url = prepend + quote(split[1].encode('utf-8')) + append
-    urls.append((split[0], url))
+    urls.append((split[0], split[1]))
+
+output = open(argv[2], 'w')
+
+for url in urls:
+  data = urlopen(url[1])
+  docs = json.load(data)['response']['docs']
+  
+  rank = 1
+  for doc in docs:
+    output.write(url[0] + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(doc['score']) + ' ' + model + '\n')
+    rank += 1
+
+output.close()
