d6bff09b4669a72f9efb8847148ba288d0609666
Alexander Simeonov <agsimeon@buffalo.edu>
2015-11-07 21:22:16 -0500

    Further progress towards a better solr to trec converter.

--- solr-trec/solrtrec.py
+++ solr-trec/solrtrec.py
@@ -12,11 +12,9 @@ if len(argv) != 5:
   exit(0)
 
 port = 8983
-prepend = "http://" + argv[3] + ":" + str(port) + "/solr/" + argv[4] + "/select?"
+prepend = "http://" + argv[3] + ":" + str(port) + "/solr/" + argv[4] + "/select?q="
 append = '&fl=id%2Cscore&wt=json&indent=true&rows=1000'
 urls = [] # (id, url) format
-
-# print quote(u"РФ в Сирии вынудили 250 тунисских боевиков бежать".encode('UTF-8'))
 
 with codecs.open(argv[1],encoding='utf-8') as queries:
   for query in queries.read().splitlines():
