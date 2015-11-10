19da52243b19077a427afce7393dc96a5a48502f
Alexander Simeonov <agsimeon@buffalo.edu>
2015-11-09 21:55:04 -0500

    Changed name of the custom query parser and it now extends Extended DisMax.

---
+++ solr-plugins/src/edu/buffalo/cse535/AgsimeonQueryParser.java
@@ -0,0 +1,7 @@
+package edu.buffalo.cse535;
+
+import org.apache.solr.search.ExtendedDismaxQParserPlugin;
+
+public class AgsimeonQueryParser extends ExtendedDismaxQParserPlugin {
+  public static final String NAME = "agsimeon";
+}
--- solr-plugins/src/edu/buffalo/cse535/CustomQueryParser.java
+++
@@ -1,7 +0,0 @@
-package edu.buffalo.cse535;
-
-import org.apache.solr.search.LuceneQParserPlugin;
-
-public class CustomQueryParser extends LuceneQParserPlugin {
-
-}
