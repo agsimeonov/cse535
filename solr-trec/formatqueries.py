ea30f29f6d4965fb6133749d0ecbd524c1bb4df9
Alexander Simeonov <agsimeon@buffalo.edu>
2015-11-16 01:18:01 -0500

    formatqueries now handles original language specification correctly.

--- solr-trec/formatqueries.py
+++ solr-trec/formatqueries.py
@@ -11,7 +11,7 @@ import goslate
 # Make sure we have the correct command line arguments
 if len(argv) != 5:
   print "Please provide command line arguments as follows:"
-  print "python solrtrec.py <Query File> <Output File> <Domain> <Core>"
+  print "python formatqueries.py <Query File> <Output File> <Domain> <Core>"
   exit(0)
 
 port = 8983
@@ -19,8 +19,9 @@ prepend = 'http://' + argv[3] + ':' + str(port) + '/solr/' + argv[4] + '/select?
 append = '&fl=id%2Cscore&wt=json&indent=true&rows='
 urls = [] # (id, url) format
 
-data = urlopen(prepend + '*' + append + '0')
-rows = json.load(data)['response']['numFound']
+# data = urlopen(prepend + '*' + append + '0')
+# rows = json.load(data)['response']['numFound']
+rows = 3440
 
 gs = goslate.Goslate(service_urls=['http://translate.google.ca',
                                    'http://translate.google.nl',
@@ -35,10 +36,20 @@ def escapeSpecialCharacters(text):
 with codecs.open(argv[1],encoding='utf-8') as queries:
   for query in queries.read().splitlines():
     split = query.split(' ', 1)
-    en = escapeSpecialCharacters(gs.translate(split[1], 'en'))
-    de = escapeSpecialCharacters(gs.translate(split[1], 'de'))
-    ru = escapeSpecialCharacters(gs.translate(split[1], 'ru'))
+    text = split[1]
+    
+    if split[1].startswith("lang:"):
+      text = text[7:]
+    
+    en = escapeSpecialCharacters(gs.translate(text, 'en'))
+    de = escapeSpecialCharacters(gs.translate(text, 'de'))
+    ru = escapeSpecialCharacters(gs.translate(text, 'ru'))
+    
     q = en + ' OR ' + de + ' OR ' + ru
+    
+    if split[1].startswith("lang:"):
+      q = split[1][0:7] + ' ' + q
+    
     q = quote(q.encode('utf-8'))
     url = prepend + q + append + str(rows)
     urls.append((split[0], url))
